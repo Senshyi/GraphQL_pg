c7e1269facbb0a242a62d6325a3e8d164610cdd1
JanMach97 <janmachacek97@gmail.com>
2018-10-31 23:54:11 +0000

    add vote

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -5,10 +5,22 @@ function newLinkSubscribe (parent, args, context, info) {
   )
 }
 
+function newVoteSubscribe (parent, args, context, info) {
+  return context.db.subscription.vote(
+    { where: { mutation_in: ['CREATED'] } },
+    info,
+  )
+}
+
 const newLink = {
   subscribe: newLinkSubscribe
 }
 
+const newVote = {
+  subscribe: newVoteSubscribe
+}
+
 module.exports = {
-  newLink
+  newLink,
+  newVote
 }
