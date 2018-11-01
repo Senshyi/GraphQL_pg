25111e249e7a852f64bdf1be48abc38328b89b2b
JanMach97 <janmachacek97@gmail.com>
2018-11-01 13:39:33 +0000

    add count to feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,4 @@
-function feed (root, args, context, info) {
+async function feed (root, args, context, info) {
   const where = args.filter
   ? {
     OR: [
@@ -8,10 +8,25 @@ function feed (root, args, context, info) {
   }
   : {}
 
-  return context.db.query.links(
-    { where, skip: args.skip, first: args.first, orderBy: args.orderBy },
-    info
+  const queriedLinks = await context.db.query.links(
+    { where, skip: args.skip, first: args.first, orderBy: args.orderBy},
+    `{ id }`,
   )
+
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
+      }
+    }
+  `
+
+  const linksConnection = await context.db.query.linksConnection({}, countSelectionSet)
+
+  return {
+    count: linksConnection.aggregate.count,
+    linkOds: queriedLinks.map(link => link.id),
+  }
 } 
 
 module.exports = {
